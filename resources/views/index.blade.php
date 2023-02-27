8561297cca86b33a3e33e56fff13b6b727191881
Eric Hunter <ehunter7@live.com>
2023-02-26 20:00:45 -0800

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+  <div class="class">Index of Ideas</div>
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>HQ Voting</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Open+Sans:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-8 py-4">
+        <a href="#">HQ Logo</a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
+            <div class="p-6 text-right">
+                @auth
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <a href="{{route('logout')}}" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </a>
+                </form>
+                @else
+                <a href="{{ route('login') }}"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                    in</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                @endif
+                @endauth
+            </div>
+            @endif
+            <a href="#">
+                <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar"
+                    class="w-10 h-10 rounded-full">
+            </a>
+        </div>
+    </header>
+</body>
+
+</html>
