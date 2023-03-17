bded543d7bedb8ba3610d3e431c06f382b8d803c
Eric Hunter <ehunter7@live.com>
2023-03-16 20:54:40 -0700

    Added more details to idea card

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,5 +37,8 @@ module.exports = {
         },
     },
 
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/line-clamp"),
+    ],
 };
