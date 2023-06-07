fa896e0d2ab2e405cdd2b6313ea88050d9f19019
Eric <ehunter7@live.com>
2023-06-07 11:22:33 -0700

    Design add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
                 175: "43.75rem",
             },
             maxWidth: {
-                custom: "62.5rem",
+                custom: "68.5rem",
             },
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
